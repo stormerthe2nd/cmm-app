d75456ce115ff5e8c9cc4c38098b97b00af6e453
Junaid Firdosi <theiconsting17@gmail.com>
2021-06-22 03:47:22 +0530

    gta 5 map added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,54 @@
 
-function myMap() {
+let map, infoWindow;
 
-  var mapProp = {
-    center: new
-      google.maps.LatLng(19.191373, 72.829863),
-    zoom: 12,
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: 19.0760, lng: 72.8777 },
     streetViewControl: false,
     mapTypeControl: false,
-    mapId: mapId || null,
-    styles: []
-  }
+    mapId: mapId || "",
+    zoom: 12,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Current Location";
+  locationButton.classList.add("custom-map-control-button", "btn", "btn-light", "mt-2", "btn-sm");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+          map.setCenter(pos);
+          new google.maps.Marker({
+            position: pos,
+            map: map,
+            icon: "https://sampwiki.blast.hk/wroot/images2/a/ad/Icon_2.gif"
+          });
+
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
